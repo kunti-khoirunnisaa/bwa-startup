5ef2ab780579434a64a30aad8ea52ce04f7e7bd4
kunti-khoirunnisaa <kunti.khoirunnisaa@gmail.com>
2021-07-10 07:09:46 +0700

    Campaign repository

--- campaign/entity.go
+++ campaign/entity.go
@@ -1,1 +1,30 @@
 package campaign
+
+import "time"
+
+// mewakili tabel campaign
+
+type Campaign struct {
+	ID               int
+	userID           int
+	Name             string
+	ShortDescription string
+	Description      string
+	Perks            string
+	BackerCount      int
+	GoalAmount       int
+	CurrentAmount    int
+	Slug             string
+	CreatedAt        time.Time
+	UpdatedAt        time.Time
+	CampaignImages   []CampaignImage
+}
+
+type CampaignImage struct {
+	ID         int
+	CampaignID int
+	FileName   string
+	IsPrimary  int
+	CreatedAt  time.Time
+	UpdatedAt  time.Time
+}
--- handler/campaign.go
+++ handler/campaign.go
@@ -1,1 +1,6 @@
 package handler
+
+// handler get parameter nil / user_id
+// handler arahin ke service
+// service panggil repo yang sesuai
+// repo
