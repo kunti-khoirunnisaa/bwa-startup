977b0fb0da7220f556a1ddc185b8e5a3c868fd1c
kunti-khoirunnisaa <kunti.khoirunnisaa@gmail.com>
2021-07-11 07:33:21 +0700

    Campaign service: endpoint formatter

--- campaign/formatter.go
+++ campaign/formatter.go
@@ -1,1 +1,38 @@
 package campaign
+
+type CampaignFormatter struct {
+	ID               int    `json:"id"`
+	UserID           int    `json:"user_id"`
+	Name             string `json:"name"`
+	ShortDescription string `json:"short_description"`
+	ImageUrl         string `json:"image_url"`
+	GoalAmount       int    `json:"goal_amount"`
+	CurrentAmount    int    `json:"current_amount"`
+}
+
+func FormatCampaign(campaign Campaign) CampaignFormatter {
+	campaignformatter := CampaignFormatter{}
+	campaignformatter.ID = campaign.ID
+	campaignformatter.UserID = campaign.userID
+	campaignformatter.Name = campaign.Name
+	campaignformatter.ShortDescription = campaign.ShortDescription
+	campaignformatter.GoalAmount = campaign.GoalAmount
+	campaignformatter.CurrentAmount = campaign.CurrentAmount
+
+	if len(campaign.CampaignImages) > 0 {
+		campaignformatter.ImageUrl = campaign.CampaignImages[0].FileName
+	}
+
+	return campaignformatter
+}
+
+func FormatCampaigns(campaigns []Campaign) []CampaignFormatter {
+	campaignsFormatter := []CampaignFormatter{}
+
+	for _, campaign := range campaigns {
+		campaignFormatter := FormatCampaign(campaign)
+		campaignsFormatter = append(campaignsFormatter, campaignFormatter)
+	}
+
+	return campaignsFormatter
+}
--- handler/campaign.go
+++ handler/campaign.go
@@ -22,10 +22,10 @@ func (h *campaignHandler) GetCampaigns(c *gin.Context) {
 
 	campaigns, err := h.service.GetCampaigns(userID)
 	if err != nil {
-		response := helper.APIResponse("Error get campaigns", http.StatusBadRequest, "error", nil)
+		response := helper.APIResponse("Error get campaigns", http.StatusBadRequest, "error", campaign.FormatCampaigns(campaigns))
 		c.JSON(http.StatusBadRequest, response)
 		return
 	}
-	response := helper.APIResponse("Succes get campaigns", http.StatusBadRequest, "error", campaigns)
+	response := helper.APIResponse("Succes get campaigns", http.StatusOK, "success", campaign.FormatCampaigns(campaigns))
 	c.JSON(http.StatusOK, response)
 }
